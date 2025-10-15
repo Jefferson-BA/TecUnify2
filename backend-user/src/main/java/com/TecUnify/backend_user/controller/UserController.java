ea016a2488ac0b769902a307ac037ae97f6b5dc9
Jefferson <jefferson.ig.ig@gmail.com>
2025-10-15 03:34:16 -0500

    a

--- backend-user/src/main/java/com/TecUnify/backend_user/controller/UserController.java
+++ backend-user/src/main/java/com/TecUnify/backend_user/controller/UserController.java
@@ -1,4 +1,43 @@
 package com.TecUnify.backend_user.controller;
+import com.TecUnify.backend_user.dto.UserDTO;
+import com.TecUnify.backend_user.model.User;
+import com.TecUnify.backend_user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserDTO> updateUser(
+            @PathVariable Long id,
+            @RequestBody User userDetails) {
+        return ResponseEntity.ok(userService.updateUser(id, userDetails));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> cancelarReserva(@PathVariable Long id) {
+        reservaService.cancelarReserva(id);
+        return ResponseEntity.ok("Reserva cancelada");
+    }<?> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok("Usuario eliminado");
+    }
 }
+
