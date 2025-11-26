3d6a29c9e97c08e1e4893de804893270cb9cb670
Jefferson <jefferson.ig.ig@gmail.com>
2025-11-26 09:15:36 -0500

    Add initial implementation of Actividad module with controller, service, repository, DTO, and event emitter classes

---
+++ backend-user/src/main/java/com/TecUnify/backend_user/events/ActividadEventEmitter.java
@@ -0,0 +1,4 @@
+package com.TecUnify.backend_user.events;
+
+public class ActividadEventEmitter {
+}
---
+++ backend-user/src/main/java/com/TecUnify/backend_user/repository/ActividadRepository.java
@@ -0,0 +1,4 @@
+package com.TecUnify.backend_user.repository;
+
+public interface ActividadRepository {
+}
